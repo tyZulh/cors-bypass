413e72b8ad9e2b248f3667f0a01f6c3ad220eef2
Thomas Paoli <34407263+tyZulh@users.noreply.github.com>
2021-10-29 17:02:43 +0200

    feat : add route for a simple http call (#2)

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 require('dotenv').config()
+
 const express = require('express');
+const axios = require('axios')
+
 const PORT = process.env.PORT;
 
 const app = express();
+
+app.get('/:url', async (req, res) => {
+  try {
+    const result = await axios.get(req.originalUrl)
+    res.status(200).json(result)
+  } catch(err) {
+    console.error(err)
+    res.status(500).send('server error')
+  }
+})
 
 app.listen(PORT, () => {
   console.log(`Server up and runnign on : ${PORT}`);
