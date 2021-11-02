681ff712229121383354dcd8b7245a48125172ba
Thomas Paoli <thomas.paoli.tp@gmail.com>
2021-11-02 09:42:31 +0100

    feat: add cors management

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 require('dotenv').config()
 
 const express = require('express');
+const cors = require('cors')
 const axios = require('axios')
 
 const PORT = process.env.PORT;
 
 const app = express();
+app.use(cors())
 
 app.use('/',async (req,res) => {
-  console.log(req.headers);
   try {
     const authorization  = req.headers.authorization ? req.headers.authorization : null 
     const result = await axios.get(`https:/${req.originalUrl}`, { headers: {authorization} } )
